1540cda60f7907ec375ec1778f2ed8091449eec8
luca.pana <luca.pana@stud.acs.upb.ro>
2022-05-24 17:45:36 +0300

    added queue interface

---
+++ queue.h
@@ -0,0 +1,17 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "list.h"
+
+typedef list queue;
+
+#define queue_alloc list_alloc
+#define queue_free list_free
+
+int enqueue(queue *self, const void *data);
+void *dequeue(queue *self);
+int queue_purge(queue *self);
+void queue_peek(queue *self);
+unsigned int queue_size(queue *self);
+
+#endif
