d863d23e25adde450c8e32b284afc472ce4c3ce0
luca.pana <luca.pana@stud.acs.upb.ro>
2022-05-26 19:18:06 +0300

    added binary tree initial interface

---
+++ bintree.h
@@ -0,0 +1,20 @@
+#ifndef BINTREE_H
+#define BINTREE_H
+
+struct bintree_node{
+	void *data;
+	struct nmbintree_node_s *right;
+	struct nmbintree_node_s *left;
+};
+
+struct bintree{
+	unsigned int size;
+	int (*cmp)(const void *e1, const void *e2);
+	void (*destructor)(void *data);
+	bintree_node *root;
+};
+
+typedef bintree_node node;
+typedef bintree tree;
+
+#endif
